5a00cceafdce2b9060c17d239215982b34df113c
RAHUL MISHRA <avaantivirus2021@gmail.com>
2024-08-13 08:08:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,6 +141,6 @@ if upload_file is not None:
         st.dataframe(emoji_df)
 
     with col2:
-        fig = go.Figure(data=[go.Pie(labels=emoji_df['emoji'].head(), values=emoji_df['count'].head(), textinfo='label+percent')])
+        fig = go.Figure(data=[go.Pie(labels=emoji_df[0].head(), values=emoji_df[1].head(), textinfo='label+percent')])
         fig.update_traces(textposition='inside', textfont_size=14)
         st.plotly_chart(fig)
